a68bfca8720dc20e12a309d1cb9cd46b53730048
oliverJBW <oliver_bw@hotmail.com>
2022-11-02 16:48:11 +0100

    l

--- cities.php
+++ cities.php
@@ -18,39 +18,63 @@ if ($error != null) {
     <title>Tema 3, bases de datos</title>
 </head>
 <body>
-    <?php
-        $limit = "";
-        if (isset($GET['n'])){
-            $limit = "LIMIT " . $_GET['n'];
-        }
-        $sql = "SELECT * FROM city" . $limit; 
-        //echo $sql;
-        $result = $link->query($sql);
-        $row = $result->fetch_array();
-        ?>
-        <table>
     
-            <th>ID</th>
-            <th>Name</th>
-            <th>CountryCode</th>
-            <th>District</th>
-            <th>Population</th>
-        
-        <?php
-        while ($row != null){
-    ?>
-        <tr>
-            <td><?=$row['ID']?></td>
-            <td><?=$row['Name']?></td>
-            <td><?=$row['CountryCode']?></td>
-            <td><?=$row['District']?></td>
-            <td><?=$row['Population']?></td>
-        </tr>
+    
     <?php 
-        $row = $result->fetch_array();
-    } 
-    ?>
-    </table>
+    if(!isset($_POST["primerEnvio"])){
+        $sql = "SELECT DISTINCT continent FROM country";
+        $consulta1  = $link->query($sql);
+        $filaConsulta1 = $consulta1->fetch_array();
+        ?>
+        <form action="cities.php" method="post">
+        <select name="continentes" id="select1">
+        <option hidden disabled selected value="">Elige un continente</option>
+        <?php
+        while ($filaConsulta1 != null) { ?>
+            <option value=""><?php echo $filaConsulta1["continent"] ?></option>
+            <?php $filaConsulta1 = $consulta1->fetch_array();?>
+        <?php } ?>    
+        </select> 
+        <input type="submit" value="Elegir" name="primerEnvio">
+        </form>
+    <?php } ?>
+    
+    
+    <?php
+        if (false){
+            $limit = "";
+            if (isset($_GET['n'])){
+                $limit = " LIMIT " . $_GET['n'];
+            }
+            $sql = ("SELECT * FROM city" . $limit); 
+            //echo $sql;
+            $result = $link->query($sql);
+            $row = $result->fetch_array();
+            ?>
+            <table>
+        
+                <th>ID</th>
+                <th>Name</th>
+                <th>CountryCode</th>
+                <th>District</th>
+                <th>Population</th>
+            
+            <?php
+                while ($row != null){
+            ?>
+                <tr>
+                    <td><?=$row['ID']?></td>
+                    <td><?=$row['Name']?></td>
+                    <td><?=$row['CountryCode']?></td>
+                    <td><?=$row['District']?></td>
+                    <td><?=$row['Population']?></td>
+                </tr>
+            <?php 
+                $row = $result->fetch_array();
+            } 
+            ?>
+            </table>
+    <?php } ?>   
     
 </body>
 </html>
